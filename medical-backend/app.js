1e5f499cba418fbdd7d099ec90181dbbec2c41d4
Oleh Vavryniv <oleh02@gmail.com>
2021-10-16 00:08:17 +0300

    add dockerfiles

--- medical-backend/app.js
+++ medical-backend/app.js
@@ -7,7 +7,6 @@ import MongoService from './mongo-service.js';
 import Axios from 'axios';
 
 dotenv.config();
-MongoService.InitConnection();
 Axios.defaults.headers.common['Token'] = process.env.SCREEN_TOKEN;
 
 const app = express();
@@ -28,11 +27,33 @@ app.listen(process.env.PORT, () => {
   console.log(`VSCREEN app listening at http://localhost:${process.env.PORT}`)
 });
 
-async function Init() {
-
+async function updateData() {
   const q = new DbUpdater();
   await q.updateData();
+}
 
+function sendTemp() {
+  try {
+    const rawTemp = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
+    // const rawTemp = 30000;
+    const temp = rawTemp / 1000;
+
+    const screenId = process.env.SCREEN_ID;
+    const host = process.env.VSCREEN_URL;
+    Axios.post(`${host}/api/${screenId}/temp_logs`, { temp: temp });
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+async function Init() {
+  await MongoService.InitConnection();
+  await updateData();
+
+  setInterval(() => updateData(), 60000);
+  setInterval(() => sendTemp(), 60000);
+ 
   console.log("App started.");
 }
 
